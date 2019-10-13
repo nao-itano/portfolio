ff5e5d0b485125d20f7ad08b23915b4dddbd1e00
np <np@nao-itano.local>
2019-10-13 23:03:54 +0900

    [add]お問い合わせ送信失敗ページの追加

---
+++ error.php
@@ -0,0 +1,21 @@
+<?php
+  /* Template Name: 送信失敗 */
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php wp_head(); ?>
+</head>
+
+</head>
+<body>
+  <div class="l-contact-page">
+    <h1 class="heading -primary">送信に失敗しました。</h1>
+    <p class="contact__content">送信に失敗しました。<br>お手数をおかけしますが、もう一度お試し頂くか、<br>下記のメールアドレスにご連絡ください。</送信に失敗しました。<br>
+  <p class="error__address"><a href="mailto:np10320@gmail.com" class="address">np10320@gmail.com</a></p>
+
+    <p><a href="http://nao-itano.com/" class="button -primary">戻る</a></p>
+  </div>
+<?php get_footer(); ?>
