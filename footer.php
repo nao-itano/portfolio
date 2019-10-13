099e853803745bb3e227a459c84ffcdd0388759f
np <np@nao-itano.local>
2019-10-13 23:28:03 +0900

    [add]WordPress用footerページ追加

---
+++ footer.php
@@ -0,0 +1,13 @@
+  <footer class="l-footer">
+    <div class="l-container">
+      <p><a href="#top" class="js-topButton button -top uppercase">top</a></p>
+      <p class="l-footer__icon">
+        <a href="https://github.com/nao-itano" target="_blank" class="footer__git"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/github-logo.png" alt="github"></a>
+      </p>
+      <p class="l-footer__copyright"><small>&copy;2019 Nao Itano</small></p>
+    </div>
+    <!-- /.l-container -->
+  </footer>
+  <?php wp_footer(); ?>
+</body>
+</html>
