0c1c4dabac0838fd6ca52db0b04477fb6b9b7b7d
np <np@nao-itano.local>
2019-10-14 00:45:05 +0900

    [add]お問い合わせ完了ページ追加

---
+++ thanks.php
@@ -0,0 +1,20 @@
+<?php
+ /* Template Name: 送信完了 */
+ ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php wp_head(); ?>
+</head>
+<body>
+  <section>
+    <div class="l-contact-page">
+      <h1 class="heading -primary">お問い合わせ完了</h1>
+      <p class="contact__content">お問い合わせありがとうございます。<br>
+      折り返しご連絡させていただきます。</p>
+      <p><a href="http://nao-itano.com/" class="button -primary">戻る</a></p>
+    </div>
+  </section>
+<?php get_footer(); ?>
